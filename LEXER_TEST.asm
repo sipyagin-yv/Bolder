fb19032469d2df3b6fa71208f85931b4f107caf6
Sipyagin Yuriy <sipyagin-yv@yandex.ru>
2016-04-03 22:37:12 +0300

    Проверка SynWrite

---
+++ LEXER_TEST.asm
@@ -0,0 +1,30 @@
+; Это проба лексера
+
+                ;
+                ; test_proc (created 03.04.2016 20:52:34)
+                ; Proc for testing purposes
+                ;
+                ; IN        -
+                ;
+                ; OUT        -
+                ;
+proc            test_proc       param1:dword, param2:dword
+                        
+
+                call    test_proc
+                
+                call    .testproc
+
+                xor     eax, eax
+                jmp     .L1
+                
+LabelTTT:                
+                
+L0:                
+                
+ L1:
+ 
+ .Local1:
+                 
+                ret        
+endp
